6cdea0d1eaed783c39da36c26bc937c0cdffcf5a
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-06-19 09:24:02 -0400

    split App into two components

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,81 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import dictionary from './util/dictionary';
-import GuessForm from './components/GuessForm';
 import './App.css';
+import Game from './components/Game';
 
 const App = () => {
   const [ answer, setAnswer ] = useState("");
-  const [ displayWord, setDisplayWord ] = useState([]);
-  const [ guessesRemaining, setGuessesRemaining ] = useState(6);
   const [ gameOver, setGameOver ] = useState(false);
 
   const getNewWord = () => {
     let newWord = dictionary[Math.floor(Math.random() * dictionary.length)];
     setAnswer(newWord);
-
-    let underscores = [];
-    for(let char of newWord) {
-      if(char === " ") {
-        underscores.push(" ");
-      } else {
-        underscores.push("_");
-      }
-    }
-    setDisplayWord(underscores);
   }
 
   useEffect(() => {
     getNewWord();
   }, [])
 
-  useEffect(() => {
+  const isGameOver = ( displayWord, answer, guessesRemaining ) => {
     if(displayWord.join("") === answer || !guessesRemaining) {
       setGameOver(true);
     } else {
       setGameOver(false);
     }
-  }, [displayWord, guessesRemaining])
-
-  const handleGuess = ( guess ) => {
-    let guessFound = false;
-    let newDisplayWord = [...displayWord];
-    for(let i = 0; i < answer.length; i ++) {
-      if(answer[i] === guess) {
-        newDisplayWord[i] = guess;
-        setDisplayWord(newDisplayWord);
-        guessFound = true;
-      }
-    }
-
-    if(!guessFound) {
-      setGuessesRemaining(guessesRemaining - 1);
-    }
   }
-
-  // useEffect(() => {
-  //   // change board
-  //   return () => {
-  //     setWord("");
-  //   }
-  // }, [word])
   
   return (
-    <div className="App">
-      { answer }
-      { gameOver ? (!guessesRemaining ? <p className="gameLose">Out of Moves!</p> : <p className="gameWin">You win!</p>) : null }
-      <p className="guessesRemaining">Guesses Remaining: {guessesRemaining}</p>
-      <div className="board">
-        { !gameOver ? displayWord.map(( letter, i ) => {
-          return (
-            <p className="wordCharacter" key={i}>
-              {letter}
-            </p>
-          )
-        }) : <p className="answer">{answer}</p> }
+      <div className="App">
+        { answer ? 
+          <Game gameOver={gameOver} setGameOver={setGameOver} isGameOver={isGameOver} answer={answer} /> :
+          <form className="gameSetup">hello</form>
+        }
       </div>
-
-      { !gameOver ? <GuessForm handleGuess={handleGuess} /> : null }
-    </div>
   )
 }
 
---
+++ hangman/src/components/Game.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import GuessForm from './GuessForm';
+import dictionary from '../util/dictionary';
+
+const Game = ({ gameOver, setGameOver, isGameOver, answer }) => {
+    const [ displayWord, setDisplayWord ] = useState([]);
+    const [ guessesRemaining, setGuessesRemaining ] = useState(6);
+
+    const getDisplayWord = () => {
+        let underscores = [];
+        for(let char of answer) {
+          if(char === " ") {
+            underscores.push(" ");
+          } else {
+            underscores.push("_");
+          }
+        }
+        setDisplayWord(underscores);
+    }
+
+    useEffect(() => {
+        getDisplayWord();
+    }, [])
+  
+    useEffect(() => {
+      isGameOver(displayWord, answer, guessesRemaining);
+    }, [displayWord, guessesRemaining])
+  
+    const handleGuess = ( guess ) => {
+      let guessFound = false;
+      let newDisplayWord = [...displayWord];
+      for(let i = 0; i < answer.length; i ++) {
+        if(answer[i] === guess) {
+          newDisplayWord[i] = guess;
+          setDisplayWord(newDisplayWord);
+          guessFound = true;
+        }
+      }
+  
+      if(!guessFound) {
+        setGuessesRemaining(guessesRemaining - 1);
+      }
+    }
+  
+    // useEffect(() => {
+    //   // change board
+    //   return () => {
+    //     setWord("");
+    //   }
+    // }, [word])
+    
+    return (
+        <div className="game">
+          { gameOver ? (!guessesRemaining ? <p className="gameLose">Out of Moves!</p> : <p className="gameWin">You win!</p>) : null }
+          <p className="guessesRemaining">Guesses Remaining: {guessesRemaining}</p>
+          <div className="board">
+            { !gameOver ? displayWord.map(( letter, i ) => {
+              return (
+                <p className="wordCharacter" key={i}>
+                  {letter}
+                </p>
+              )
+            }) : <p className="answer">{answer}</p> }
+          </div>
+  
+          { !gameOver ? <GuessForm handleGuess={handleGuess} /> : null }
+        </div>
+    )
+}
+
+export default Game;
